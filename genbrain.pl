45b2574aa2a1fa6f99de5c1052415bb5144bcd43
Raoul Zwart <datamoeras@gmail.com>
2012-01-30 11:55:30 +0100

    tests for parser stage

--- genbrain.pl
+++ genbrain.pl
@@ -1,16 +1,19 @@
 #!/usr/bin/perl
 
-use lib '/home/raoul/jp';
 use genbrain;
 
+use Cwd qw/getcwd/;
+use vars qw/$path/;
+BEGIN { $path = getcwd() . '/'. $0; $path =~ s{/\./}{/}g; $path =~ s/\/[^\/]+$/\//; unshift @INC, $path; };
 use strict;
 use warnings;
+use autodie qw/open/;
 use JSON;
 use Mojo::DOM;
 use Data::Dumper;
 
 my %opt;
-if ($ARGV[0] =~ /^-j$/) {
+if ($ARGV[0] && $ARGV[0] =~ /^-j$/) {
 	shift;
 	$opt{json}++;
 }
@@ -21,6 +24,30 @@ if ($ARGV[0]) {
 	if ($opt{json}) {
 		print JSON->new->encode($data);
 	} else {
-		print Dumper($data);
+		#print Dumper($data);
+		testoutput($data);
 	}
+}
+
+sub testoutput {
+	my $data = shift;
+	open(my $fh, '>', $_) for <$path/tst/*txt>;
+	for my $b (@{ $data }) {
+		tstbrn($b);
+	}
+}
+
+sub tstbrn {
+	my $data = shift;
+	my $title = $data->{title};
+	open(my $fh, '>', "$path/tst/$title.txt");
+	my $i = 0;
+	no warnings;
+	for my $track (@{ $data->{list}}) {
+		my $tm = $track->[0];
+		my $info = $track->[1];
+		$i++;
+		print $fh join("\t", $i, "$tm->[0]:$tm->[1]", "artist=$info->{artist}", "title=$info->{title}"), "\n";
+	}
+	close($fh);
 }
